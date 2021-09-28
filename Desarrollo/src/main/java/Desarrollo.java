454a257603dff7f43366ad9ac28d3e2835f9f883
Nacho <i.vallejos01@ufromail.cl>
2021-09-28 09:40:45 -0300

    -README

--- Desarrollo/src/main/java/Desarrollo.java
+++ Desarrollo/src/main/java/Desarrollo.java
@@ -1,3 +1,23 @@
+import java.util.Random;
+
 public class Desarrollo {
+
+    public static void generarArreglo(int [] arr){
+        Random rnum= new Random();
+        int n= rnum.nextInt(8)+2;
+        arr=new int[n];
+        for (int i = 0; i < arr.length; i++){
+            arr[i]=rnum.nextInt(2000)-1000;
+            System.out.print("[" +arr[i]+ "]");
+            System.out.println(" ");
+        }
+    }
+
+    public static void mostrarParesAdyacentes(int[] arreglo) {
+        System.out.println("Los pares adyacentes son: ");
+        for (int i = 1; i < arreglo.length; i++) {
+            System.out.println("(" + arreglo[i-1] +","+arreglo[i]+")");
+        }
+    }
 
 }
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Lab02
 integrantes: Belén Sánchez - Ignacio Vallejos
 
+Belén: 
+mostrarMenu()
+menuAccion()
+generarArreglo()
+
+Ignacio:
+mostrarParesAdyacentes()
+buscarProductoMayor()
